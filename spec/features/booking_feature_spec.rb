e78350527f524c49a8a0d6447f412702b1f2b7dd
Joseph Knowles <joejknowles@gmail.com>
2017-01-16 21:51:23 +0000

    change test name

--- spec/features/booking_feature_spec.rb
+++ spec/features/booking_feature_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 require_relative '../../config/urls.rb'
 
 feature 'Booking' do
-  scenario "should say today's date" do
+  scenario "should say today" do
     visit Urls::SLOTS
-    expect(page).to have_content 'Today '
+    expect(page).to have_content 'Today'
   end
 end
