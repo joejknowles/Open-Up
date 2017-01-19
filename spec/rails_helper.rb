be23a815b7ffe18d2677772c5e640a96cf3992a5
Joseph Knowles <joejknowles@gmail.com>
2017-01-19 00:28:36 +0000

    remove js errors from poltergeist for CI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,9 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/poltergeist'
 
-Capybara.javascript_driver = :poltergeist
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, js_errors: false)
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
