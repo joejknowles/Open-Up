4b8d1d062a10f4e5c3e03dc13d10aedc5c4f15f6
Joseph Knowles <joejknowles@gmail.com>
2017-01-19 09:12:00 +0000

    add poltergeist

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/poltergeist'
 
+Capybara.javascript_driver = :poltergeist
+
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, js_errors: false)
 end
