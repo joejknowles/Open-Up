258f4ad7647455f2dd7ceeac7f62721c56193365
Joseph Knowles <joejknowles@gmail.com>
2017-01-19 00:03:10 +0000

    setup js to run in tests using Poltergeist - PhantomJS driver

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'jbuilder', '~> 2.5'
 group :test do
   gem 'rspec-rails'
   gem 'capybara'
+  gem 'poltergeist'
   gem 'coveralls', require: false
 end
 
--- spec/features/booking_feature_spec.rb
+++ spec/features/booking_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require_relative '../../config/urls.rb'
 
-feature 'Booking' do
+feature 'Booking', js: true do
   scenario "should say today" do
     visit Urls::SLOTS
     expect(page).to have_content 'Today'
