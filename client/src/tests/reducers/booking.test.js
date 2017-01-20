dbe7c448f7efd4b9c815031ac8d03b1760bdcefa
Joseph Knowles <joejknowles@gmail.com>
2017-01-20 14:42:18 +0000

    add slotsById test

--- client/src/tests/reducers/booking.test.js
+++ client/src/tests/reducers/booking.test.js
@@ -13,3 +13,11 @@ it('slotsById returns empty object by default', () => {
   const actual = slotsById(null, {});
   expect(actual).toEqual({});
 });
+
+it('slotsById returns slots by Id', () => {
+  const action = { response:
+    { slots: [{ id: 1 }, {id: 2}] }
+  };
+  const actual = slotsById(null, action);
+  expect(actual).toEqual({ 1: { id: 1 }, 2: {id: 2} });
+});
