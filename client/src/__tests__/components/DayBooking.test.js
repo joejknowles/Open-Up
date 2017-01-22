5b7383d02cbf5fc8bc081c535413520cd8e1b7d9
Joseph Knowles <joejknowles@gmail.com>
2017-01-22 10:09:00 +0000

    no slots unit test

--- client/src/__tests__/components/DayBooking.test.js
+++ client/src/__tests__/components/DayBooking.test.js
@@ -6,13 +6,31 @@ import renderer from 'react-test-renderer';
 
 const fakeDate = '1999-01-01';
 
-it('renders booking component', () => {
-  const app = shallow(<DayBooking
+it('renders two buttons for two slots', () => {
+  const component = shallow(<DayBooking
     date={ fakeDate }
     slots={ [{ id: 1 }, { id: 2 }] }
     fetchSlots={ ()=> {}}
     />);
-  expect(app.find('button').length).toBe(2);
+  expect(component.find('button').length).toBe(2);
+});
+
+it('renders three buttons for three slots', () => {
+  const component = shallow(<DayBooking
+    date={ fakeDate }
+    slots={ [{ id: 1 }, { id: 2 }, { id: 4 }] }
+    fetchSlots={ ()=> {}}
+    />);
+  expect(component.find('button').length).toBe(3);
+});
+
+it('renders message for no slots', () => {
+  const component = shallow(<DayBooking
+    date={ fakeDate }
+    slots={ [] }
+    fetchSlots={ ()=> {}}
+    />);
+  expect(component.find('.message').text()).toBe('No slots available');
 });
 
 it('renders correctly', () => {
