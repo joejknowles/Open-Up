bb9528e0855944dab2c793f6f78e6d6c56621e8c
Joseph Knowles <joejknowles@gmail.com>
2017-01-24 22:15:56 +0000

    end loading reducer test

--- client/src/__tests__/reducers/loading.test.js
+++ client/src/__tests__/reducers/loading.test.js
@@ -1,3 +1,7 @@
 import isLoading from '../../reducers/isLoading';
 
 it('isLoading defaults to true', () => expect(isLoading(undefined, {})).toBe(true));
+
+it('isLoading becomes false when FETCH_**_SUCCESS action called', () => (
+  expect(isLoading(true, { type: 'FETCH_SLOTS_SUCCESS' })).toBe(false)
+));
