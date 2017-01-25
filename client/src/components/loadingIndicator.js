8a5c314456c27d24535c0f42a8047dc696168858
Joseph Knowles <joejknowles@gmail.com>
2017-01-25 09:54:37 +0000

    tweak loading indicator

--- client/src/components/loadingIndicator.js
+++ client/src/components/loadingIndicator.js
@@ -8,15 +8,17 @@ export default () => {
           key: i,
           transform: `rotate(${ i * 30 } 50 50) translate(0 -30)`
         },
-        animateProps: { begin: `${ i / 12.0 }s` }
+        animateProps: { begin: `${ i / 24.0 }s` }
       });
   }
-  return (<svg width='120px' height='120px' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" className="uil-default">
-    <rect x="0" y="0" width="100" height="100" fill="none" className="bk"></rect>
-    { spokes.map((spoke) => (
-      <rect  x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00b2ff' { ...spoke.rectProps}>
-          <animate attributeName='opacity' from='1' to='0' dur='1s' { ...spoke.animateProps } repeatCount='indefinite'/>
+  return (
+    <svg style={ { marginTop: '200px' }} width='120px' height='120px' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" className="uil-default">
+      <rect x="0" y="0" width="100" height="100" fill="none" className="bk"></rect>
+      { spokes.map((spoke) => (
+        <rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='black' { ...spoke.rectProps}>
+          <animate attributeName='opacity' from='1' to='0' dur='0.5s' { ...spoke.animateProps } repeatCount='indefinite'/>
         </rect>
       )) }
-  </svg>);
+    </svg>
+  );
 };
