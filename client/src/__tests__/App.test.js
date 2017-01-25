c55085b7350c62d6c228ed701b63fb5413e933f0
Joseph Knowles <joejknowles@gmail.com>
2017-01-25 15:36:23 +0000

    refactor app test

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -7,17 +7,18 @@ import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 
 const mockStore = configureStore([]);
+const emptyStoreMock = mockStore({ allSlots: [] });
 
 it('renders without crashing', () => {
-  shallow(<App store={ mockStore({ allSlots: [] }) }/>);
+  shallow(<App store={ emptyStoreMock }/>);
 });
 
 it('renders booking component', () => {
-  const app = shallow(<App store={ mockStore({ allSlots: [] }) }/>);
+  const app = shallow(<App store={ emptyStoreMock }/>);
   expect(app.find(DayBooking).length).toBe(1);
 });
 
 it('renders correctly', () => {
-  const tree = renderer.create(<App store={ mockStore({ allSlots: [] }) }/>).toJSON();
+  const tree = renderer.create(<App store={ emptyStoreMock }/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
