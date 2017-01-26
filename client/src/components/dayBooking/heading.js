c721b1ef138722bca547bea4b5201892fd0d7225
Joseph Knowles <joejknowles@gmail.com>
2017-01-26 11:01:30 +0000

    re-use date selector in heading

--- client/src/components/dayBooking/heading.js
+++ client/src/components/dayBooking/heading.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectedDateSelector } from '../../reducers';
 
 export const Heading = ({date}) => (
   <h3>Today { date }</h3>
 );
 
-export const mapStateToProps = ({ selectedDate }) => ({
-  date: selectedDate
+export const mapStateToProps = (state) => ({
+  date: selectedDateSelector(state)
 });
 
 export default connect(mapStateToProps)(Heading);
