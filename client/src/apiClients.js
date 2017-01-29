3214b7c9e4c287e53c40bcb6381eb9c82f14cfe4
Joseph Knowles <joejknowles@gmail.com>
2017-01-29 15:39:06 +0000

    remove console log

--- client/src/apiClients.js
+++ client/src/apiClients.js
@@ -2,10 +2,6 @@ import 'whatwg-fetch';
 
 export const fetchSlots = () => {
   return fetch('/api/slots').then(
-    (response) => {
-      const asJson = response.json()
-      console.log(asJson);
-      return asJson;
-    }
+    (response) => response.json()
   );
 };
