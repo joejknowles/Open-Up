f0bb9149eb5efd99d6683c010c33bb7a8d62c6b8
Joseph Knowles <joejknowles@gmail.com>
2017-01-29 21:14:15 +0000

    implement post client for booking

--- client/src/__tests__/apiClients.test.js
+++ client/src/__tests__/apiClients.test.js
@@ -1,15 +1,49 @@
 import * as apiClients from '../apiClients';
 
-window.fetch = jest.fn((url) => (
-  new Promise((resolve, reject) => (
-    url === '/api/slots' ?
-    resolve({ json: () => ({ slots: [] }) }) :
-    reject()
-  ))
-));
+const setFetchFunction = () => {
+  window.fetch = jest.fn((url) => (
+    new Promise((resolve, reject) => (
+      resolve({ json: () => ({ slots: [] }) })
+    ))
+  ));
+}
+
+it('fetchSlots to return response as json', () => {
+  setFetchFunction();
+  return apiClients.fetchSlots().then((response) => {
+    return expect(response).toEqual({ slots: [] });
+  });
+});
 
 it('fetchSlots to call fetch with /api/slots url', () => {
+  window.fetch.mockReset();
+  setFetchFunction();
   return apiClients.fetchSlots().then((response) => {
+    return expect(window.fetch).toHaveBeenCalledWith('/api/slots');
+  });
+});
+
+it('bookSlot to call fetch with /api/booking url', () => {
+  window.fetch.mockReset();
+  setFetchFunction();
+  const slotId = 1;
+  const url = '/api/booking';
+  const options = {
+    method: 'POST',
+    headers: { "Content-Type": "aplication/json" },
+    body: JSON.stringify({ slotId })
+  };
+  const expectedArgs = [url, options]
+  return apiClients.bookSlot(slotId).then((response) => {
+    return expect(window.fetch).toHaveBeenCalledWith(...expectedArgs);
+  });
+});
+
+it('bookSlot to call return response json', () => {
+  window.fetch.mockReset();
+  setFetchFunction();
+
+  return apiClients.bookSlot(1).then((response) => {
     return expect(response).toEqual({ slots: [] });
   });
 });
--- client/src/apiClients.js
+++ client/src/apiClients.js
@@ -1,7 +1,17 @@
 import 'whatwg-fetch';
 
-export const fetchSlots = () => {
-  return fetch('/api/slots').then(
+export const fetchSlots = () => (
+  fetch('/api/slots').then(
     (response) => response.json()
-  );
-};
+  )
+);
+
+export const bookSlot = (slotId) => (
+  fetch('/api/booking', {
+    method: 'POST',
+    headers: { "Content-Type": "aplication/json" },
+    body: JSON.stringify({ slotId })
+  }).then(
+    (response) => response.json()
+  )
+);
