033a09421062423ce8e672c5a5e0873c41da4f08
Joseph Knowles <joejknowles@gmail.com>
2017-01-29 12:05:16 +0000

    add default slot seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def add_default_slots
+  start_hours = [
+    9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+  ]
+  start_hours.each do |start_hour|
+    start_time = Time.new(1970, 10, 10, start_hour + 1)
+    end_time = Time.new(1970, 10, 10, start_hour + 2)
+    Slot.create(start_time: start_time, end_time: end_time)
+  end
+end
+
+add_default_slots if Slot.all.empty?
