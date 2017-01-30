a9ede77fd8fce2c5ee712cf90508f735c686b905
Joseph Knowles <joejknowles@gmail.com>
2017-01-30 23:42:01 +0000

    duplicate sot error

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 describe Booking, type: :model do
   it "is invalid without a slot" do
-    slot = Booking.new()
-    slot.valid?
-    expect(slot.errors[:slot].size).to be(1)
+    booking = Booking.new()
+    booking.valid?
+    expect(booking.errors[:slot].size).to be(1)
+  end
+
+  it "is invalid with slot that's already booked" do
+    slot_id = Slot.create(start_time: Time.now, end_time: Time.now).id
+    booking1 = Booking.new(slot_id: slot_id)
+    booking2 = Booking.new(slot_id: slot_id)
+    booking2.valid?
+    expect(booking2.errors[:slot].size).to be(1)
   end
 end
