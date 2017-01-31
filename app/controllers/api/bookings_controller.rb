339e36ebd7b3e9fb8eca64378eafc98443f27030
Joseph Knowles <joejknowles@gmail.com>
2017-01-31 10:02:51 +0000

    refactor booking controller

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,9 +1,8 @@
 class Api::BookingsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
-    booking = Booking.new()
-    if booking.valid?
-      booking.save
+    booking = Booking.new(slot_id_params)
+    if booking.save
       render json: { booking_id: booking.id }
     else
       render json: { errors: booking.errors.full_messages }, status: 422
