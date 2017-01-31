0eb093da4cc571773dd17eff5ca78fdf22891a63
Joseph Knowles <joejknowles@gmail.com>
2017-01-31 11:31:42 +0000

    finish errors reducer

--- client/src/__tests__/reducers/errors.test.js
+++ client/src/__tests__/reducers/errors.test.js
@@ -2,8 +2,8 @@ import errors from '../../reducers/errors';
 
 it('errors defaults to empty array', () => expect(errors(undefined, {})).toEqual([]));
 
-it('adds error message when BOOK_SLOTS_FAILURE action called', () => (
-  expect(errors([], { type: 'BOOK_SLOTS_FAILURE', response: { errors: ['booking failure'] } })).toEqual(['booking failure'])
+it('adds error message when BOOK_SLOT_FAILURE action called', () => (
+  expect(errors([], { type: 'BOOK_SLOT_FAILURE', response: { errors: ['booking failure'] } })).toEqual(['booking failure'])
 ));
 
 it('adds error message when FETCH_SLOTS_FAILURE action called', () => (
--- client/src/reducers/errors.js
+++ client/src/reducers/errors.js
@@ -1,5 +1,10 @@
+const isFailureType = ({ type }) => (
+  type === 'FETCH_SLOTS_FAILURE' ||
+  type === 'BOOK_SLOT_FAILURE'
+);
+
 export default (state = [], action) => {
-  if (action.type === 'FETCH_SLOTS_FAILURE') {
+  if (isFailureType(action)) {
     return [
       ...state,
       ...action.response.errors
