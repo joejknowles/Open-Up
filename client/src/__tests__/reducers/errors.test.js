43708790135182a3add5461e85ab221f63a91f63
Joseph Knowles <joejknowles@gmail.com>
2017-01-31 11:25:35 +0000

    add errors reducer tests

---
+++ client/src/__tests__/reducers/errors.test.js
@@ -0,0 +1,11 @@
+import errors from '../../reducers/errors';
+
+it('errors defaults to empty array', () => expect(errors(undefined, {})).toEqual([]));
+
+it('adds error message when BOOK_SLOTS_FAILURE action called', () => (
+  expect(errors([], { type: 'BOOK_SLOTS_FAILURE', response: { errors: ['booking failure'] } })).toEqual(['booking failure'])
+));
+
+it('adds error message when FETCH_SLOTS_FAILURE action called', () => (
+  expect(errors(['other error message'], { type: 'FETCH_SLOTS_FAILURE', response: { errors: ['slot failure'] } })).toEqual(['other error message', 'slot failure'])
+));
