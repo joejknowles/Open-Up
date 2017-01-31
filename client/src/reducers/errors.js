23d5761207994481bb18ae14bbd0397ca563b6a0
Joseph Knowles <joejknowles@gmail.com>
2017-01-31 11:29:20 +0000

    fetch slots failure handled in errors reducer

--- client/src/reducers/errors.js
+++ client/src/reducers/errors.js
@@ -1,3 +1,9 @@
 export default (state = [], action) => {
+  if (action.type === 'FETCH_SLOTS_FAILURE') {
+    return [
+      ...state,
+      ...action.response.errors
+    ];
+  }
   return state;
 };
