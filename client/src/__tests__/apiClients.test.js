301002e34326f54c13056bcea9f54859a77a389c
Joseph Knowles <joejknowles@gmail.com>
2017-01-31 00:06:13 +0000

    extract common api test code

--- client/src/__tests__/apiClients.test.js
+++ client/src/__tests__/apiClients.test.js
@@ -8,42 +8,34 @@ const setFetchFunction = () => {
   ));
 }
 
-it('fetchSlots to return response as json', () => {
-  setFetchFunction();
-  return apiClients.fetchSlots().then((response) => {
-    return expect(response).toEqual({ slots: [] });
+const testApiClient = (clientName, arg, expectedArgs) => {
+  it(clientName + ' to return response as json', () => {
+    setFetchFunction();
+    return apiClients[clientName](arg).then((response) => {
+      expect(response).toEqual({ slots: [] });
+      window.fetch.mockReset();
+    });
   });
-});
 
-it('fetchSlots to call fetch with /api/slots url', () => {
-  window.fetch.mockReset();
-  setFetchFunction();
-  return apiClients.fetchSlots().then((response) => {
-    return expect(window.fetch).toHaveBeenCalledWith('/api/slots');
+  it(clientName + ' to call fetch with correct arguments', () => {
+    setFetchFunction();
+    return apiClients[clientName](arg).then((response) => {
+      expect(window.fetch).toHaveBeenCalledWith(...expectedArgs);
+      window.fetch.mockReset();
+    });
   });
-});
+}
 
-it('bookSlot to call fetch with /api/booking url', () => {
-  window.fetch.mockReset();
-  setFetchFunction();
-  const slotId = 1;
-  const url = '/api/booking';
-  const options = {
-    method: 'POST',
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ slot_id: slotId })
-  };
-  const expectedArgs = [url, options]
-  return apiClients.bookSlot(slotId).then((response) => {
-    return expect(window.fetch).toHaveBeenCalledWith(...expectedArgs);
-  });
-});
+testApiClient('fetchSlots', null, ['/api/slots']);
 
-it('bookSlot to call return response json', () => {
-  window.fetch.mockReset();
-  setFetchFunction();
 
-  return apiClients.bookSlot(1).then((response) => {
-    return expect(response).toEqual({ slots: [] });
-  });
-});
+const slotId = 1;
+const url = '/api/booking';
+const options = {
+  method: 'POST',
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ slot_id: slotId })
+};
+const expectedArgs = [url, options]
+
+testApiClient('bookSlot', slotId, expectedArgs )
