ae8b14d3e8cb42af8ea1835d1a1657008c9029a1
Joseph Knowles <joejknowles@gmail.com>
2017-02-02 10:17:06 +0000

    dispatch nextDay action

--- client/src/components/dayBooking/heading.js
+++ client/src/components/dayBooking/heading.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { selectedDateSelector } from '../../reducers';
 import format from 'date-fns/format'
+import { nextDay } from '../../actions/changeDay';
 
 const dateTitle = (date) => (
   `Today ${format(date, 'D/M/YYYY')}`
 );
 
-export const Heading = ({ date }) => (
+export const Heading = ({ date, onNextDayClick }) => (
   <div className="Heading">
     <h3>{ dateTitle(date) }</h3>
-    <a className="next-day">Next Day</a>
+    <a
+      className="next-day"
+      onClick={ onNextDayClick }>
+      Next Day
+    </a>
   </div>
 );
 
@@ -18,4 +23,6 @@ export const mapStateToProps = (state) => ({
   date: selectedDateSelector(state)
 });
 
-export default connect(mapStateToProps)(Heading);
+const mapDispatchToProps = (dispatch) => ({ onNextDayClick: () => dispatch(nextDay) });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Heading);
