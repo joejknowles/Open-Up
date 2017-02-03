8383f76d4f5017eb718ea4a640f2a7790bf3e8a6
Joseph Knowles <joejknowles@gmail.com>
2017-02-03 11:51:22 +0000

    format initial state

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -2,12 +2,15 @@ import topReducer from './reducers';
 import { createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
+import addDays from 'date-fns/add_days';
 
 export default () => {
   const middleware = [ thunk ];
   if (process.env.NODE_ENV !== 'production') {
     middleware.push(createLogger());
   }
-  const initialState = { selectedDate: new Date() };
+  const initialState = {
+    selectedDate: new Date()
+  };
   return createStore(topReducer, initialState, applyMiddleware(...middleware));
 };
