2a2e1e72b7e87b2844057b3e782a77ca760874dc
Joseph Knowles <joejknowles@gmail.com>
2017-02-05 09:34:01 +0000

    make slot_id unique

--- db/migrate/20170129130433_create_bookings.rb
+++ db/migrate/20170129130433_create_bookings.rb
@@ -1,7 +1,7 @@
 class CreateBookings < ActiveRecord::Migration[5.0]
   def change
     create_table :bookings do |t|
-      t.belongs_to :slot, foreign_key: true
+      t.belongs_to :slot, foreign_key: true, unique: true
 
       t.timestamps
     end
