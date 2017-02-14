25b3103b44dd26614c942e4811ff6d6a9abe2115
Joseph Knowles <joejknowles@gmail.com>
2017-02-14 12:09:55 +0000

    close modal

--- client/src/components/common/modal.js
+++ client/src/components/common/modal.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import '../../styles/Modal.css';
+import { exitBooking } from '../../helpers/routing';
 
 export default ({ children }) => (
-  <div className="modal">
-    <div className="modal-box">
+  <div className="modal" onClick={ exitBooking } >
+    <div className="modal-box" onClick={ (e) => e.stopPropagation() }>
       { children }
     </div>
   </div>
